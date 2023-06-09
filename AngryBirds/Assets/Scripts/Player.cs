84d147f71215a1a7edbb980f417fa49bbce695f9
paulamgonzalez <114227774+Dxvah@users.noreply.github.com>
2023-06-09 13:33:04 +0100

    unity no es mi amigo

--- AngryBirds/Assets/Scripts/Player.cs
+++ AngryBirds/Assets/Scripts/Player.cs
@@ -2,34 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    
     public Rigidbody2D rb;
     public Vector2 startPos;
-   
    // public Collider2D col;
-
-
      void Start()
      {
 
         rb.GetComponent<Rigidbody2D>();
         startPos = transform.position;
         //col.GetComponent<Collision2D>();
+        //Collider2D[] listaPuntosChoque = Physics2D.OverlapCircleAll(startPos, 15);
+        //Debug.Log(listaPuntosChoque.Length);     
+     }
+    private void Update()
+    {
         Collider2D[] listaPuntosChoque = Physics2D.OverlapCircleAll(startPos, 15);
-
-        Debug.Log(listaPuntosChoque.Length);
-
-        for ( int i = 0 ; i < listaPuntosChoque.Length ; i++ )
+        if (!Touchscreen.current.primaryTouch.press.isPressed)
         {
-            Collider2D objetoChocado = listaPuntosChoque[i];
-            Rigidbody2D rbObjetoChocado = objetoChocado.gameObject.GetComponent<Rigidbody2D>();
-            rbObjetoChocado.AddForce(new Vector3(100,100,0), ForceMode2D.Force);
+            Debug.Log("ha Tocado");
+            //for (int i = 0; i < listaPuntosChoque.Length; i++)
+            {
+                //Collider2D objetoChocado = listaPuntosChoque[i];
+                //Rigidbody2D rbObjetoChocado = objetoChocado.gameObject.GetComponent<Rigidbody2D>();
+                //rbObjetoChocado.AddForce(new Vector3(1, 1, 0), ForceMode2D.Force);
+            }
         }
-
-
     }
     void OnCollisionEnter2D(Collision2D col)
     {
@@ -43,6 +44,6 @@ public class Player : MonoBehaviour
     {
         // Draw a yellow sphere at the transform's position
         
-        Gizmos.DrawSphere(transform.position, 15);
+        Gizmos.DrawSphere(transform.position, 1);
     }
 }
